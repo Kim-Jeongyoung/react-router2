2eaf3c9b6361ab5842c96d306e86060176962f54
Jeongyoung Kim <jeongyoung2@gmail.com>
2022-05-23 08:23:03 -0700

    284. Implementing Programmatic (Imperative) Navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,17 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    //     push: allows users to go back
+    // replace: replace the current page (redirect)
+
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
